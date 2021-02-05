1c4348da0386fc371ee5b67123aab97cfd2f6a7b
siddhi188 <73110622+siddhi188@users.noreply.github.com>
2021-02-05 17:00:13 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,30 @@
+class Block {
+    constructor(x,y,width,height) {
+      var options = {
+          restitution : 0.8
+
+      }
+      var visibility = 255
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      if(this.body.speed < 3){
+      var pos =this.body.position;
+      push()
+      translate(pos.x , pos.y) 
+      rotate(this.body.angle )
+      rectMode(CENTER)
+      rect(0,0,this.width,this.height)
+      pop()
+      }
+      else{
+        World.remove(world,this.body)
+        push()
+        this.visibility--
+        pop()
+      }
+    }
+  };
